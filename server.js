9a635c1734ab4a0437bb3d95493e8400d5472263
CucciPro <josej@email.arizona.edu>
2020-09-11 20:05:17 -0700

    added fetch for user input

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+//make css and script avaialable
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -113,4 +111,20 @@ app.get('/api/animals/:id', (req, res) => {
         const animal = createNewAnimal(req.body, animals)
         res.json(animal);
     }
-  });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
